d615bb1111e11761c85935a29ba981da29488e3a
Axel Villa <axelvilla746@gmail.com>
2024-06-30 20:45:45 -0300

    Finalizacion de pagina, a modificar

--- app/app.py
+++ app/app.py
@@ -6,39 +6,117 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
+    data = {'tituloPagina': 'Autor único'}
     if request.method == 'POST':
-        data = {
-        'apellido': apellido,
-        'nombre': nombre,
-        'anio': anio,
-        'titulo': titulo,
-        'editorial': editorial,
-    }
         apellido = request.form['Apellido']
         nombre = request.form['Nombre']
         anio = request.form['Anio']
         titulo = request.form['Titulo']
         editorial = request.form['Editorial']
-    
+        data = {
+            'tituloPagina': 'Autor único',
+            'apellido': apellido.capitalize(),
+            'nombre': nombre[0:1].capitalize(),
+            'anio': anio,
+            'titulo': titulo.capitalize(),
+            'editorial': editorial.capitalize(),
+        }
     return render_template('index.html', data = data)
 
 @app.route('/institucional', methods = ['GET', 'POST'])
 def institucional():
+    data = {'tituloPagina': 'Autor Institucional'}
+    if request.method == 'POST':
+        institucion = request.form['institucion']
+        anio = request.form['Anio']
+        titulo = request.form['Titulo']
+        editorial = request.form['Editorial']
+        data = {
+            'tituloPagina': 'Autor Institucional',
+            'institucion': institucion.capitalize(),
+            'anio': anio,
+            'titulo': titulo.capitalize(),
+            'editorial': editorial.capitalize(),
+        }
+    return render_template('institucional.html', data = data)
+
+@app.route('/libros', methods = ['GET', 'POST'])
+def libros():
+    data = {'tituloPagina': 'Libros Electronicos'}
     if request.method == 'POST':
         apellido = request.form['Apellido']
         nombre = request.form['Nombre']
         anio = request.form['Anio']
         titulo = request.form['Titulo']
         editorial = request.form['Editorial']
+        url = request.form['url']
         data = {
-            'apellido': apellido,
-            'nombre': nombre,
+            'tituloPagina': 'Libros Electronicos',
+            'apellido': apellido.capitalize(),
+            'nombre': nombre[0:1].capitalize(),
             'anio': anio,
-            'titulo': titulo,
-            'editorial': editorial,
+            'titulo': titulo.capitalize(),
+            'editorial': editorial.capitalize(),
+            'url': url
         }
-    return render_template('institucional.html', data = data)
+    return render_template('libros_electronicos.html', data = data)
+
+@app.route('/web_autor', methods = ['GET', 'POST'])
+def web_autor():
+    data = {'tituloPagina': 'Sitio Web con autor'}
+    if request.method == 'POST':
+        apellido = request.form['Apellido']
+        nombre = request.form['Nombre']
+        fecha = request.form['fecha']
+        titulo = request.form['Titulo']
+        nombreSitio = request.form['nombreSitio']
+        url = request.form['url']
+        data = {
+            'tituloPagina': 'Sitio Web con autor',
+            'apellido': apellido.capitalize(),
+            'nombre': nombre[0:1].capitalize(),
+            'fecha': fecha,
+            'titulo': titulo.capitalize(),
+            'nombreSitio': nombreSitio.capitalize(),
+            'url': url
+        }
+    return render_template('web_autor.html', data = data)
+
+@app.route('/web_no_autor', methods = ['GET', 'POST'])
+def web_no_autor():
+    data = {'tituloPagina': 'Sitio Web sin autor'}
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        fecha = request.form['fecha']
+        nombreSitio = request.form['nombreSitio']
+        url = request.form['url']
+        data = {
+            'tituloPagina': 'Sitio Web sin autor',
+            'fecha': fecha,
+            'titulo': titulo.capitalize(),
+            'nombreSitio': nombreSitio.capitalize(),
+            'url': url
+        }
+    return render_template('web_no_autor.html', data = data)
+
+@app.route('/video_internet', methods = ['GET', 'POST'])
+def video_internet():
+    data = {'tituloPagina': 'Video de Internet'}
+    if request.method == 'POST':
+        canal = request.form['canal']
+        fecha = request.form['fecha']
+        titulo = request.form['Titulo']
+        nombreSitio = request.form['nombreSitio']
+        url = request.form['url']
+        data = {
+            'tituloPagina': 'Video de Internet',
+            'canal': canal,
+            'fecha': fecha,
+            'titulo': titulo.capitalize(),
+            'nombreSitio': nombreSitio.capitalize(),
+            'url': url
+        }
+    return render_template('video_internet.html', data = data)
 
 
 
